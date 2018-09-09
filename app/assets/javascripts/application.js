2d1f648411861bacf357ff7f408ed6706bcb448a
pascales <pascales.dedy@gmail.com>
2018-09-10 04:09:19 +0700

    new layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,14 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
